b535b917b08afecd2e07ea641f1b509ac2ad054b
KevDonovan <66144711+KevDonovan@users.noreply.github.com>
2023-09-24 20:46:28 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,30 @@
+let dropDown = document.querySelectorAll('.dropdownMenu');
+let navLinks = document.querySelectorAll('.navLink');
+let smallNav = document.querySelector('.smallNav');
+let smallNavContent = document.querySelector('.navDropContent')
+
+
+smallNav.addEventListener('click', function() {toggleSmallDrop()});
+
+navLinks.forEach((e, i) => {
+    e.addEventListener('mouseover', function() {openDropdown(i)});
+    e.addEventListener('mouseout', function() {closeDropdown(i)});
+})
+
+dropDown.forEach((e, i) => {
+    e.addEventListener('mouseover', function() {openDropdown(i)});
+    e.addEventListener('mouseout', function() {closeDropdown(i)});
+})
+
+function openDropdown(i) {
+    if(!dropDown[i].classList.contains('active')) dropDown[i].classList.add('active');
+}
+
+function closeDropdown(i) {
+    dropDown[i].classList.remove('active');
+}
+
+function toggleSmallDrop() {
+    if(!smallNavContent.classList.contains('active')) smallNavContent.classList.add('active');
+    else smallNavContent.classList.remove('active');
+}
